10984260c2e1f3bc2ca68a96dd717145abbb5931
bhoirprathmesh2004@gmail.com <bhoirprathmesh2004@gmail.com>
2024-09-13 12:29:56 +0530

    mern

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -8,6 +8,9 @@
 //  and send responses back to clients. They help organize your application by 
 //  separating concerns and following the MVC (Model-View-Controller) design pattern.
 
+const User = require("../models/user-model");
+const bcrypt = require("bcryptjs");
+
 // *----------------------
 //* Home Page Logic
 // *----------------------
@@ -22,10 +25,41 @@ const home = async (req, res) => {
 // *----------------------
 //* Register Logic
 // *----------------------
+// 1. Get Registration Data:  Retrieve user data (username, enail, password).
+// 2. Check Email Existence: Check if the email is already registered.
+// 3. Hash Password: Securely hash the password.
+// 4. Create User: Create a new user with hashed password. 
+// 5. Save to DB: Save user data to the database. 
+// 6. Respond: Respond with "Registration Successful or handle errors.
+
 const register = async (req, res) => {
     try {
         console.log(req.body);
-        res.status(200).json({ message: req.body });
+        const { username, email, phone, password } = req.body;
+
+        const userExist = await User.findOne({ email });
+        if(userExist) {
+            return res.status(400).json({ msg: "Email already exists" });
+        }
+
+        // hash the password
+        // const saltRound = 10;  // can pass any value higher it value make more complex and time consuming.
+        // const hash_password = await bcrypt.hash(password, saltRound);
+        
+        const userCreated = await User.create({
+            username, 
+            email, 
+            phone, 
+            password /*:hash_password*/
+        });
+
+        res.status(201).json({ msg : "Registration Successful", 
+            token: await userCreated.generateToken(), 
+            userId: userCreated._id.toString(),
+        }); 
+        // In most cases, converting id to a string is a good practice because it ensures consistency 
+        // and compatibility across different JWT libraries and systems. It also aligns with the 
+        // expectation that claims in a JWT are represented as strings.
     } catch (error) {
         res.status(500).json("Internal Server Error");
     }
